2d0fd876ed77fefadab67fca64cdaee3991df6f5
Gareth Sanashee <sanashee05@hotmail.com>
2018-03-27 15:45:03 +0100

    adding book reviews

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,10 +3,11 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/home'; 
 import BookView from './components/Books';
-import Layout from './hoc/layout';
 import Login from './containers/Admin/login'; 
 import User from './components/Admin';
+import AddReview from './containers/Admin/add';
 
+import Layout from './hoc/layout';
 import Auth from './hoc/auth';
 
 //switch - acts accordingly to which ever route is requested 
@@ -17,6 +18,7 @@ const Routes = () => {
                 <Route path="/" exact component={Auth (Home, null)} />
                 <Route path="/login" exact component={Auth (Login, false)} />
                 <Route path="/user" exact component={Auth(User,true)}/>
+                <Route path="/user/add" exact component={Auth(AddReview,true)}/>
                 <Route path="/books/:id" exact component={Auth (BookView)} />
             </Switch>
         </Layout>
