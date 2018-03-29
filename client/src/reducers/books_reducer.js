2322c32c238cb1c3da23d05490d7ddcaed187efe
Gareth Sanashee <sanashee05@hotmail.com>
2018-03-29 15:04:12 +0100

    edit and deleting book reviews

--- client/src/reducers/books_reducer.js
+++ client/src/reducers/books_reducer.js
@@ -6,12 +6,14 @@ export default function(state={},action){
         case 'GET_BOOKS':
         //return previous state [...state] inside [list] where [action.payload] shows the books 
             return {...state, list: action.payload}
+        case 'GET_BOOK':
+            return {...state, book:action.payload}
         case 'GET_BOOK_W_REVIEWER':
             return {
-                    ...state, 
-                    book: action.payload.book,
-                    reviewer: action.payload.reviewer
-                }
+                ...state,
+                book:action.payload.book,
+                reviewer:action.payload.reviewer
+            }
         case 'CLEAR_BOOK_W_REVIEWER':
             return {
                     ...state, 
@@ -22,7 +24,23 @@ export default function(state={},action){
             return {...state,newbook: action.payload}
         case 'CLEAR_NEWBOOK':
             return {...state,newbook:action.payload}
-
+        case 'UPDATE_BOOK':
+            return {...state,
+                 updateBook: action.payload.success,
+                 book: action.payload.doc
+                }
+        case 'DELETE_BOOK':
+            return {
+                ...state,
+                postDeleted: action.payload
+            }
+        case 'CLEAR_BOOK':
+            return {
+                ...state,
+                updateBook:action.payload.updateBook,
+                book:action.payload.book,
+                postDeleted:action.payload.postDeleted
+            }
         default:
             return state;
     }
